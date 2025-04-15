5bf4116463d0ddf032072fc8b421675b2f6c9cbd
Sahib Manjal <ssm237@cornell.edu>
2025-04-15 00:40:56 -0400

    Klingus Borkus IV

--- DGEMM/DGEMM.ino
+++ DGEMM/DGEMM.ino
@@ -1,19 +1,47 @@
 int in1Pin = 11;
 int in2Pin = 9;
 int enablePin = 6;
+int homePin = 5;
 
 byte latchTime = 75;
 byte flipState = LOW;
+byte isHome;
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(in1Pin, OUTPUT);
   pinMode(in2Pin, OUTPUT);
   pinMode(enablePin, OUTPUT);
+  pinMode(homePin, INPUT);
+  Serial.begin(9600);
+  singleFlip();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // singleFlip();
+  goHome();
+}
+
+void goHome() {
+  // Move to Home Position
+  for (int i = 0; i <= 255; i++) {
+    isHome = digitalRead(homePin);
+    Serial.println(isHome);
+    Serial.println("----------------");
+
+    if (isHome == LOW) {
+      singleFlip();
+    }
+    else {
+      delay(10000);
+      break;
+    }
+    delay(50);
+  }
+}
+
+void singleFlip() {
+  // Advances a Single Flip
   flipState = flipState == HIGH ? LOW : HIGH;
 
   digitalWrite(enablePin, HIGH);
@@ -21,5 +49,5 @@ void loop() {
   digitalWrite(in2Pin, !flipState);
   delay(latchTime);
   digitalWrite(enablePin, LOW);
-  delay(5000);
+  delay(1000);
 }
