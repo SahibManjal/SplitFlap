ed6a9e9f63fb3230bd623856000698fb6b12e0b1
polybeandip <akashdhiraj2019@gmail.com>
2025-12-15 00:11:24 -0500

    SSE + fat unroll

---
+++ Server/server.py
@@ -0,0 +1,15 @@
+import json
+from chooseTimetable import getTimetable
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+
+class Serv(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(json.dumps(getTimetable()).encode('utf-8'))
+
+
+if __name__ == '__main__':
+    httpd = HTTPServer(('localhost', 8080), Serv)
+    httpd.serve_forever()
